dc4f718ff8a744dee4947fa4889e0a2e8d5165a8
abigailtakyi1 <2425404308@live.gctu.edu.gh>
2026-05-31 16:26:53 +0000

    Add files via upload
    Scripting Language Assignment 2
    Name: Abigail Asantewaa Takyi 
    Index number: 2425404308
    Programme: Cyber Security (group B)

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints out my initials (AAT) in stylized large letters.
+# Assignment Number: 1
+#
+# Name: ABIGAIL ASANTEWAA TAKYI
+# STUDENT ID:  2425404308
+# Email: 2425404308@live.gctu.edu.gh
+# Grader: AUGUSTUS BUCKMAN
+#
+# On my honor, ABIGAIL ASANTEWAA TAKYI, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This function handles the formatted output of the stylized initials.
+    print()
+    print("...AAT")
+    print()
+    print(".......AAAA................AAAA............TTTTTTTTTTTT......")
+    print("......AA..AA..............AA..AA...........TTTTTTTTTTTT......")
+    print(".....AA....AA............AA....AA..............TTTT..........")
+    print("....AA......AA..........AA......AA.............TTTT..........")
+    print("...AA........AA........AA........AA............TTTT..........")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA............TTTT..........")
+    print("...AAA......AAA........AAA......AAA............TTTT..........")
+    print("...AAA......AAA........AAA......AAA............TTTT..........")
+    print("...AAA......AAA...**...AAA......AAA...**.......TTTT........**")
+    print("...AAA......AAA...**...AAA......AAA...**.......TTTT........**")
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
